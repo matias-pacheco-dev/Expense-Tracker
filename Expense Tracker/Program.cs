feafe94d432b4fafed52f9ed36621e82dd928379
Matias Pacheco <franckk2002@hotmail.com>
2025-02-07 15:42:37 -0300

    Testing

--- Expense Tracker/Program.cs
+++ Expense Tracker/Program.cs
@@ -7,17 +7,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+    options.UseSqlServer(Environment.GetEnvironmentVariable("DATABASE_URL")));
 
 SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NMaF5cXmBCfEx0R3xbf1x1ZFBMYl5bRXRPIiBoS35Rc0ViW3xfd3ZXR2RaU0d/");
 
 var app = builder.Build();
 
+// Aplica las migraciones automáticamente al iniciar la aplicación
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    dbContext.Database.Migrate(); 
+    dbContext.Database.Migrate();
 }
 
 // Configure the HTTP request pipeline.
